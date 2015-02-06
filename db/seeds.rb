f79cd632438135d1c98ea522e47ac2a4736b96d0
katrina gardner <yutzeng0314@gmail.com>
2015-02-06 15:45:28 -0500

    rails

---
+++ db/seeds.rb
@@ -0,0 +1,14 @@
+Blog.create(title: "BOORMAN", content: "There aren’t many filmmakers who can match John Boorman’s visual imagination.")
+
+Blog.create(title: "CHIMES AT MIDNIGHT", content: "A FILM TO TREASURE. Welles brings several passages of genius")
+
+
+
+
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
